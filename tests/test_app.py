b7ec4e3dbb089c238fa89385f7f2c1d2e6efd6ed
ccppurcell <ccppurcell@gmail.com>
2025-11-17 18:47:22 +0100

    adds test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,42 @@
+import pygame
+import networkx as nx
+from claw_hunter.main import ClawHunter
+
+def test_add_node():
+
+    #init the app
+    app = ClawHunter()
+    assert app.id == 1
+    
+    #set the points
+    point_1 = (0,0)
+    point_2 = (1,1)
+
+    #add nodes
+    app.add_node(point_1)
+    app.add_node(point_2)
+
+    assert app.id==3
+
+    #set another point
+    point_3 = (2,2)
+
+    app.edge_queue=[1]
+
+    app.add_node(point_3)
+    assert app.edge_queue == []
+
+    positions = nx.get_node_attributes(app.G, "pos")
+    assert app.pos == positions
+
+def test_add_edge():
+
+    #init the app
+    app = ClawHunter()
+
+    #add some nodes
+    app.add_node((0,0))
+    app.add_node((1,1))
+
+    #add an edge
+    app.add_edge(1,2)
